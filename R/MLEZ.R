5b1bd2fd51c00ec3a3edfd221f1cbb55e97476c2
dazamora <dazamoraa@unal.edu.co>
2016-10-25 14:56:39 -0500

    Comment Description file and MLEZ function.

--- R/MLEZ.R
+++ R/MLEZ.R
@@ -1,20 +1,29 @@
-#' Title
+#' MLEZ
+#' 
+#' This function fitted parameters of probability distribution functions (see \code{\link{selecDIST}})
+#' by means of maximum likelihood and Performs evolutionary global optimization via the 
+#' Differential Evolution algorithm (see \pkg{DEoptim} package).
 #'
-#' @param x 
-#' @param type 
-#' @param para.int 
-#' @param silent 
-#' @param null.on.not.converge 
+#' @param x: Intensity values 
+#' @param type: a character specifying the name of distribution function that it will 
+#' be employed: exponencial, gamma, gev, gumbel, log.normal3, normal, pearson, log.pearson3 and 
+#' wakeby (see \code{\link{selecDIST}}).
+#' @param para.int: Initial parameters as a vector \eqn{\Theta}}.
+#' @param silent: A logical to silence the \code{\link{try}}() function wrapping the \code{\link{DEoptim}}() function.
+#' @param null.on.not.converge: A logical to trigging simple return of NULL if the optim() function returns a nonzero convergence status.
 #' @param ... 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-MLEZ <- function (x, type, para.int = NULL, silent = TRUE, null.on.not.converge = TRUE, 
+MLEZ <- function (Intensity, type, para.int = NULL, silent = TRUE, null.on.not.converge = TRUE, 
                 ptransf = function(t) return(t), pretransf = function(t) return(t), 
                 ...) 
 {
+  x <- Intensity
+  type <- selecDIST(Type = type)
+  
   if (is.null(para.int)) {
     lmr <- lmomco::lmoms(x)
     para.int <- lmomco::lmom2par(lmr, type = type, ...)
@@ -42,8 +51,8 @@ MLEZ <- function (x, type, para.int = NULL, silent = TRUE, null.on.not.converge 
   total.par <- abs(para.int$para)
   lower <- para.int$para*0.7*(-1)
   upper <- para.int$para*1.3
-  try(rt <- DEoptim(afunc, lower, upper, x = x, 
-                    control = DEoptim.control(NP = 50, itermax = 100, trace = FALSE, packages = "lmomco")),silent = silent)
+  try(rt <- DEoptim::DEoptim(afunc, lower, upper, x = x, 
+                    control = DEoptim::DEoptim.control(NP = 50, itermax = 100, trace = FALSE, packages = "lmomco")),silent = silent)
   
   if (is.null(rt)) {
     warning("optim() attempt is NULL")
